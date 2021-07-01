d3e5b83b564f8f5a68c0a5b0b60d779f8601293e
hackcoderr <sachinkumarkashyap9025@gmail.com>
2021-07-01 15:07:31 +0530

    k8s options are added

--- js/style.js
+++ js/style.js
@@ -19,3 +19,17 @@ const options = {
 
 const darkmode = new Darkmode(options);
 darkmode.showWidget();
+
+// login page
+
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
